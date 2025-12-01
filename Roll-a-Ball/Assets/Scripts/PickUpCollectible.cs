055d27c36e115684c40edecea13652254c7c75c5
Stampe1 <phillip.stampe@gmail.com>
2025-12-01 17:32:28 +0100

    Added game

---
+++ Roll-a-Ball/Assets/Scripts/PickUpCollectible.cs
@@ -0,0 +1,28 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickUpCollectible : MonoBehaviour
+{
+    public int score;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            // Add code to handle the pickup logic, e.g., increase score, play sound, etc.
+            score++;
+            Debug.Log("Collectible picked up! Score: "+score);
+            Destroy(gameObject); // Remove the collectible from the scene
+        }
+
+        
+      
+    }
+}
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms.Impl;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public int score;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue MovementValue)
+    {
+        Vector2 MovementVector = MovementValue.Get<Vector2>();
+
+        movementX = MovementVector.x;
+        movementY = MovementVector.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+
+            // Add code to handle the pickup logic, e.g., increase score, play sound, etc.
+            score++;
+            Debug.Log("Collectible picked up! Score: " + score);
+            Destroy(other.gameObject); // Remove the collectible from the scene
+        }
+
+
+
+    }
+}
